5cef62eb1025e8a39affdee159de2869bee14408
Jatin099 <56469086+Jatin099@users.noreply.github.com>
2022-01-10 16:54:33 +0530

    Create 1.Two_Sum.java

---
+++ 1.Two_Sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> indexMap = new HashMap<Integer, Integer>();
+        for(int i=0; i<nums.length;i++ ){
+            int req = (int)(target-nums[i]);
+            if(indexMap.containsKey(req)){
+                int toReturn[] = {indexMap.get(req),i};
+                return toReturn;
+            }
+            indexMap.put(nums[i],i);
+        }
+        return null;
+    }
+}
