e99614baecbf8b95e0713e972f4abdd48acd4597
Jatin Jain <56469086+Jatin099@users.noreply.github.com>
2022-05-10 19:29:06 +0530

    Time: 1 ms (73.61%), Space: 41.3 MB (64.49%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        solve(1,k,n, new ArrayList<>(), res);
+        return res;
+    }
+    
+    public void solve(int start, int k , int n, List<Integer> temp, List<List<Integer>> res){
+        if(n == 0 && temp.size() == k){
+            res.add(new ArrayList<>(temp));
+        }
+        for(int i=start; i<=9; i++){
+            temp.add(i);
+            solve(i+1, k, n-i, temp, res);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
