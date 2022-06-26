45b40c7dd23381496c069c2ef9769cdf8a3c7aa1
Jatin Jain <56469086+Jatin099@users.noreply.github.com>
2022-06-26 13:22:38 +0530

    Time: 2 ms (94.06%), Space: 50.6 MB (93.63%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,21 +1,38 @@
 class Solution {
-    public int maxScore(int[] cardPoints, int k) {
-       int max = 0;
+//     public int maxScore(int[] cardPoints, int k) {
+//        int max = 0;
         
-        for(int i =0; i<k; i++){
-            max += cardPoints[i];
-        }
+//         for(int i =0; i<k; i++){
+//             max += cardPoints[i];
+//         }
         
-        int curr = max;
-        for(int i = k-1; i>=0; i--){
-            curr -= cardPoints[i];
-            curr += cardPoints[cardPoints.length - k+i];
+//         int curr = max;
+//         for(int i = k-1; i>=0; i--){
+//             curr -= cardPoints[i];
+//             curr += cardPoints[cardPoints.length - k+i];
             
             
-            max = Math.max(curr, max);
-        }
+//             max = Math.max(curr, max);
+//         }
         
         
-        return max;
-    }
+//         return max;
+//     }
+
+
+public int maxScore(int[] cardPoints, int k) {
+	int n = cardPoints.length, totalSum = 0, minSubarray = 0;
+
+	for (int i=0, currSum=0; i<n; i++) {
+		totalSum += cardPoints[i];
+		currSum += cardPoints[i];
+		if (i < n-k) minSubarray += cardPoints[i];
+		else {
+			currSum -= cardPoints[i-(n-k)];
+			minSubarray = Math.min(minSubarray, currSum);
+		}
+	}
+
+	return totalSum - minSubarray;
+}
 }
