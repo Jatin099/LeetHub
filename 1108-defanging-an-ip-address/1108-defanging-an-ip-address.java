5ed03b6e509e2396d5d1ff5fbd0d970979e0633e
Jatin Jain <56469086+Jatin099@users.noreply.github.com>
2022-07-02 23:46:29 +0530

    Time: 19 ms (5.46%), Space: 42.9 MB (5.91%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -18,4 +18,21 @@ class Solution {
         return ne;
     }
 }
-        
+
+/*
+    class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder result = new StringBuilder();   
+        for(int i = 0 ; i < address.length() ; i++){
+            char current = address.charAt(i);
+            if(current == '.'){
+                result.append("[.]");
+            }
+            else{
+                result.append(current);
+            }
+        }
+        return result.toString();
+    }
+}
+*/
